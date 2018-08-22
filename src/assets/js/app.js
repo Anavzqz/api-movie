f17b28d183d905fd36a0e5edfc2b1d0f30fdbe3a
Ana Vzqz <ana.maria.vl@hotmail.com>
2018-08-22 18:34:04 -0500

    Pintando en pantalla de acuerdo al buscador

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,8 @@
 //manejo del DOM
 const search = document.querySelector('input');
-const btnAven = document.querySelector('btnAven');
-const btnStar = document.querySelector('btnStar');
-const btnGot = document.querySelector('btnGot');
+const btnAven = document.querySelector('#btnLord');
+const btnStar = document.querySelector('#btnStar');
+const btnGot = document.querySelector('#btnGot');
 
 search.addEventListener("keypress", (event) => {
     let key = event.which || event.keyCode;
@@ -16,8 +16,26 @@ search.addEventListener("keypress", (event) => {
 
 //traer los botones y pasar la data solo de lo que va a buscar el boton
 //al terminar con esos tres botones creo una funcion donde itere en los arreglos para traer el titulo y el posterde la pelicula
-btnAven.addEventListener('click')
+btnLord.addEventListener('click', (event) => {
+    renderInfo('the lord of the rings');
+});
 
-const renderInfo = (response) =>{
-console.log("hola");
+btnStar.addEventListener('click', (event) => {
+    renderInfo('star wars');
+});
+
+btnGot.addEventListener('click', (event) => {
+    renderInfo('game of thrones');
+});
+
+const renderInfo = (data) =>{
+    let info = document.getElementById('info');
+    let search = data.Search;
+    console.log(search);
+    for (let i = 0; i < search.length; i++){
+        info.innerHTML += `
+        <p>${search[i].Title}</p>
+        <img src="${search[i].Poster}" alt="">
+        `;
+    }
 };
