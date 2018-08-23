5d425e410735023e5d35ca758ccb0c5e5f77d09b
Ana Vzqz <ana.maria.vl@hotmail.com>
2018-08-23 11:35:03 -0500

    Corrigiendo errores en los botones

--- src/js/app.js
+++ src/js/app.js
@@ -16,26 +16,29 @@ search.addEventListener("keypress", (event) => {
 
 //traer los botones y pasar la data solo de lo que va a buscar el boton
 //al terminar con esos tres botones creo una funcion donde itere en los arreglos para traer el titulo y el posterde la pelicula
-btnLord.addEventListener('click', (event) => {
-    renderInfo('the lord of the rings');
-});
-
-btnStar.addEventListener('click', (event) => {
-    renderInfo('star wars');
-});
-
-btnGot.addEventListener('click', (event) => {
-    renderInfo('game of thrones');
-});
 
 const renderInfo = (data) =>{
     let info = document.getElementById('info');
+    info.innerHTML = '';
     let search = data.Search;
-    console.log(search);
+    
     for (let i = 0; i < search.length; i++){
         info.innerHTML += `
-        <p>${search[i].Title}</p>
+        <h5>${search[i].Title}</h5>
         <img src="${search[i].Poster}" alt="">
         `;
     }
+    
 };
+
+btnLord.addEventListener('click', (event) => {
+    getMovies('The Lord of the Rings');
+});
+
+btnStar.addEventListener('click', (event) => {
+    getMovies('Star Wars');
+});
+
+btnGot.addEventListener('click', (event) => {
+    getMovies('Game of Thrones');
+});
